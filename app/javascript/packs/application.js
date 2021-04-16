93bbe61a77afb6f0c589e585ae3b102e586948a1
marcelabrenes <marce89455@gmail.com>
2021-04-16 14:35:31 -0600

    Create and destroy working properly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,34 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import index from '../src/index'
+import show from '../src/show'
+import destroy from "../src/destroy"
+import _new from "../src/new"
+import create from "../src/create"
+
+index()
+
+document.addEventListener('click', function(e){
+    if (e.target.matches('.post_show')){
+        e.preventDefault()
+        show(e)
+    }
+    if (e.target.matches('.post_destroy')){
+        e.preventDefault()
+        destroy(e)
+    }
+    if (e.target.matches('.post_new')){
+        e.preventDefault()
+        _new()
+    }
+})
+
+document.addEventListener('submit', function(e){
+    e.preventDefault()
+    if (e.target.matches('#post_create')){
+        e.preventDefault()
+        create(e)
+    }
+})
